c5f26e7f977f71798e677d027a22a8c5526f9087
Jenny <junikab@gmail.com>
2025-03-21 12:58:10 +1100

    domain fixes

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import ContactPage from '../views/ContactPage.vue'
 import AboutPage from '../views/AboutPage.vue'
 import HomePage from '../views/HomePage.vue'
@@ -28,7 +28,7 @@ const routes = [
   // },
 ]
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory('/jennys-flowers/'),
   routes
 })
 export default router
